d4c686008d7080f71aba50a43e625700a1be8486
Ishan Singhal <ishansinghal03@gmail.com>
2025-07-28 12:41:54 +0530

    done 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Demo1 from './components/Demo1'
 import Demo2 from './components/Demo2'
 import Demo3 from './components/Demo3'
+import UI from './components/UI'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Demo1/>
     <Demo2/>
     <Demo3/>
+    
     </div>
   )
 }
---
+++ src/components/UI.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UI(){
+  return (
+    <div>
+        <h1>User login page</h1>
+    </div>
+  )
+}
+
+export default UI;
