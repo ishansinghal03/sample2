fe39b10995138db8f1404b3f461f40b46d9ef6ee
koushikeppalapally <koushikeppalapally2003@gmail.com>
2025-07-28 12:14:38 +0530

    demo3 updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Demo1 from './components/Demo1'
 import Demo2 from './components/Demo2'
+import Demo3 from './components/Demo3'
 
 const App = () => {
   return (
     <div>
     <Demo1/>
     <Demo2/>
+    <Demo3/>
     </div>
   )
 }
