0db2aee4ebdca5d3e8325097f8fbfd344276fc4d
Ishan Singhal <ishansinghal03@gmail.com>
2025-07-28 12:07:54 +0530

    demo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Demo1 from './components/Demo1'
+import Demo2 from './components/Demo2'
 
 const App = () => {
   return (
     <div>
     <Demo1/>
+    <Demo2/>
     </div>
   )
 }
