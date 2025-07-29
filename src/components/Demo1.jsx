bc3c45955620923b5284962e919d42aa2c186ea2
koushikeppalapally <koushikeppalapally2003@gmail.com>
2025-07-29 11:16:32 +0530

    deleted demos

--- src/components/Demo1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Demo1 = () => {
-  return (
-    <div>Demo1</div>
-  )
-}
-
-export default Demo1
--- src/components/Demo4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Demo4 = () => {
-  return (
-    <div>Demo4</div>
-  )
-}
-
-export default Demo4
