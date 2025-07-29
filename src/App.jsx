7fc7c08f67d3451c39e23327fff06f6fa66f68cf
Ishan Singhal <ishansinghal03@gmail.com>
2025-07-29 12:49:07 +0530

    just delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import Landing from "./components/Landing/Landing"
-import LoginForm from "./components/Login_Page"
-=======
-import React from 'react'
->>>>>>> bd500425ee03eb2bc2009d579c5b5661a6fc9ed8
+import React from "react"
 
-
-const App = () => {
+function App  () {
   return (
     <div>
-<<<<<<< HEAD
-
 <Landing />
-=======
-        
-        
->>>>>>> bd500425ee03eb2bc2009d579c5b5661a6fc9ed8
+
     </div>
   )
 }
